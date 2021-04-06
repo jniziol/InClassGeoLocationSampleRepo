41a616e9719657ded7983fedd1b76e24022ae14b
Johnathan Niziol <johnathan.niziol@mitt.ca>
2021-04-06 13:02:18 -0500

    stuff

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let latitude, longitude;
 navigator.geolocation.getCurrentPosition(function(position) {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
-  
   console.log(position);
   
 }, function(error) {
