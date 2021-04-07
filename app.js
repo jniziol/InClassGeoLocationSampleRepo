f28b8908fd6c56e7433216007acb1ceb704a40cd
Johnathan Niziol <johnathan.niziol@mitt.ca>
2021-04-07 12:54:24 -0500

    Added more important stuff

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ navigator.geolocation.getCurrentPosition(function(position) {
   longitude = position.coords.longitude;
     console.log(position);
 }, function(error) {
+  // This is important stuff
     console.warn(error);
 }, {enableHighAccuracy: true, timeout: 10000, maximumAge: 0});
 
